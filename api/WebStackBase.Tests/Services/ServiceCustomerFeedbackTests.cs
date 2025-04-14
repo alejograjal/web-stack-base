ffd2d63ce5e8a11c840b65cb0f19fc713173abf7
alejograjal <alejandro.grajal.s@gmail.com>
2025-04-14 01:07:39 -0600

    feat: complete customer feedback tests

--- api/WebStackBase.Tests/Services/ServiceCustomerFeedbackTests.cs
+++ api/WebStackBase.Tests/Services/ServiceCustomerFeedbackTests.cs
@@ -3,8 +3,10 @@ using FluentAssertions;
 using FluentValidation;
 using AutoFixture.Xunit2;
 using WebStackBase.Infrastructure;
+using WebStackBase.Domain.Exceptions;
 using WebStackBase.Application.RequestDtos;
 using WebStackBase.Application.ResponseDtos;
+using WebStackBase.Domain.Core.Specifications;
 using WebStackBase.Application.Core.Interfaces;
 using WebStackBase.Application.Services.Implementations;
 
@@ -48,4 +50,88 @@ public class ServiceCustomerFeedbackTests
         _mockCoreService.Verify(m => m.AutoMapper.Map<ResponseCustomerFeedbackDto>(customerFeedback), Times.Once);
     }
 
+    [Theory]
+    [AutoData]
+    public async Task GetAllAsync_ShouldReturn_CustomerFeedbackDtoList(
+        IList<CustomerFeedback> customerFeedbackList,
+        ICollection<ResponseCustomerFeedbackDto> responseDtoList
+    )
+    {
+        _mockCoreService.Setup(r => r.UnitOfWork.Repository<CustomerFeedback>().ListAllAsync()).ReturnsAsync(customerFeedbackList);
+        _mockCoreService.Setup(m => m.AutoMapper.Map<ICollection<ResponseCustomerFeedbackDto>>(customerFeedbackList)).Returns(responseDtoList);
+
+        // Act
+        var result = await _service.GetAllAsync();
+
+        // Assert
+        result.Should().BeEquivalentTo(responseDtoList);
+        _mockCoreService.Verify(r => r.UnitOfWork.Repository<CustomerFeedback>().ListAllAsync(), Times.Once);
+        _mockCoreService.Verify(m => m.AutoMapper.Map<ICollection<ResponseCustomerFeedbackDto>>(customerFeedbackList), Times.Once);
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task GetByIdAsync_ShouldThrowNotFound_WhenNotFound(long id)
+    {
+        _mockCoreService.Setup(repo => repo.UnitOfWork.Repository<CustomerFeedback>().FirstOrDefaultAsync(It.IsAny<BaseSpecification<CustomerFeedback>>()))
+            .ReturnsAsync((CustomerFeedback)null);
+
+        // Act
+        Func<Task> action = async () => await _service.GetByIdAsync(id);
+
+        // Assert
+        await action.Should().ThrowAsync<NotFoundException>().WithMessage("Feedback not found");
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task GetByIdAsync_ShouldReturn_CustomerFeedbackDto(long id, CustomerFeedback customerFeedback, ResponseCustomerFeedbackDto responseDto)
+    {
+        _mockCoreService.Setup(repo => repo.UnitOfWork.Repository<CustomerFeedback>().FirstOrDefaultAsync(It.IsAny<BaseSpecification<CustomerFeedback>>()))
+            .ReturnsAsync(customerFeedback);
+        _mockCoreService.Setup(m => m.AutoMapper.Map<ResponseCustomerFeedbackDto>(customerFeedback)).Returns(responseDto);
+
+        // Act
+        var result = await _service.GetByIdAsync(id);
+
+        // Assert
+        result.Should().BeEquivalentTo(responseDto);
+        _mockCoreService.Verify(repo => repo.UnitOfWork.Repository<CustomerFeedback>().FirstOrDefaultAsync(It.IsAny<BaseSpecification<CustomerFeedback>>()), Times.Once);
+        _mockCoreService.Verify(m => m.AutoMapper.Map<ResponseCustomerFeedbackDto>(customerFeedback), Times.Once);
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task UpdateAsync_ShouldThrowNotFound_WhenNotFound(long id, RequestCustomerFeedbackDto request)
+    {
+        _mockCoreService.Setup(repo => repo.UnitOfWork.Repository<CustomerFeedback>().ExistsAsync(id)).ReturnsAsync(false);
+
+        // Act
+        Func<Task> action = async () => await _service.UpdateAsync(id, request);
+
+        // Assert
+        await action.Should().ThrowAsync<NotFoundException>().WithMessage("Feedback not found");
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task UpdateAsync_ShouldReturn_UpdatedCustomerFeedbackDto(long id, RequestCustomerFeedbackDto request, CustomerFeedback customerFeedback, ResponseCustomerFeedbackDto responseDto)
+    {
+        _mockCoreService.Setup(repo => repo.UnitOfWork.Repository<CustomerFeedback>().ExistsAsync(id)).ReturnsAsync(true);
+        _mockCoreService.Setup(m => m.AutoMapper.Map<CustomerFeedback>(request)).Returns(customerFeedback);
+        _mockCoreService.Setup(repo => repo.UnitOfWork.Repository<CustomerFeedback>().Update(customerFeedback, true));
+        _mockCoreService.Setup(repo => repo.UnitOfWork.SaveChangesAsync());
+        _mockCoreService.Setup(repo => repo.UnitOfWork.Repository<CustomerFeedback>().FirstOrDefaultAsync(It.IsAny<BaseSpecification<CustomerFeedback>>()))
+            .ReturnsAsync(customerFeedback);
+        _mockCoreService.Setup(m => m.AutoMapper.Map<ResponseCustomerFeedbackDto>(customerFeedback)).Returns(responseDto);
+
+        // Act
+        var result = await _service.UpdateAsync(id, request);
+
+        // Assert
+        result.Should().BeEquivalentTo(responseDto);
+        _mockCoreService.Verify(repo => repo.UnitOfWork.Repository<CustomerFeedback>().Update(customerFeedback, true), Times.Once);
+        _mockCoreService.Verify(repo => repo.UnitOfWork.SaveChangesAsync(), Times.Once);
+        _mockCoreService.Verify(m => m.AutoMapper.Map<ResponseCustomerFeedbackDto>(customerFeedback), Times.Once);
+    }
 }
