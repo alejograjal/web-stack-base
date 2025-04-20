39e37a61577f6121503acc74bc0bf1c15b3f3347
alejograjal <alejandro.grajal.s@gmail.com>
2025-04-20 06:45:35 -0600

    fix: image server allow

--- site-public/next.config.ts
+++ site-public/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['picsum.photos', 'localhost'],
+    domains: ['picsum.photos', 'localhost', '187.33.146.62'],
   },
   output: 'standalone',
 };
